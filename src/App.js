af617cfae55dc0ffb9b2aef6dd93c195fc09b7c4
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-09-17 16:10:50 -0400

    added an animation from lootie library

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { Container, Row, } from 'react-bootstrap';
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 import Button from './components/Button.js';
 import GetPuns from './components/GetPuns.js';
+import Lottie from 'react-lottie';
+import * as animationData from './happy-woman.json';
 import './App.css';
 
-
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData
+}
 
 function App() {
   return (
     <div className='parent'>
         <h2 className='title'>Hey, that's not Punny!</h2>
         <GetPuns />
+        <Lottie options={defaultOptions} width={50} height={50}/>
     </div>
   );
 }
